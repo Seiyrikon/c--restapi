8d6189fbe6d3ec8bb6f3688c1c545ae778363653
Seiyrikon <rickygalpo7777@gmail.com>
2024-06-26 15:50:04 +0800

    commit

---
+++ Models/TodoContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using TodoApi.Models;
+
+namespace TodoApi.Modles;
+
+public class TodoContext : DbContext
+{
+    public TodoContext(DbContextOptions<TodoContext> options) : base(options)
+    {
+        
+    }
+
+    public DbSet<TodoItem> TodoItems {get; set;} = null!;
+}
